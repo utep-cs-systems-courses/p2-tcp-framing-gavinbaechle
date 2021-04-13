368f1afb8bd3a672fde51b3cbc7067afd5f25bdc
gavinbaechle <77751057+gavinbaechle@users.noreply.github.com>
2021-04-13 12:33:03 -0500

    Update clientFile.py

--- file_transfer/clientFile.py
+++ file_transfer/clientFile.py
@@ -1,101 +1,64 @@
 #! /usr/bin/env python3
 
-# Echo client program
 import socket, sys, re, time, os
 sys.path.append("../lib")       # for params
 import params
+import framedSocket
 
+progname = "fileClient"
 
-def create_socket(host):
-    switchesVarDefaults = (
-        (('-s', '--server'), 'server', "127.0.0.1:50001"),
-        (('-d', '--delay'), 'delay', "0"),
-        (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
+try:                            # tries to get the necessary parameters
+    clientFile = sys.argv[1]        
+    serverHost, serverFile = re.split(":", sys.argv[2])
+    serverPort = 50001
+except:                         # if it fails, prints syntax.
+    print("Bad param format: '%s'. Should be $ ./fileClient Send {clientFile} {host:serverFile}\n" % sys.argv)
+    sys.exit(1)
 
-    progname = "framedClient"
-    paramMap = params.parseParams(switchesVarDefaults)
-
-    server, usage  = paramMap["server"], paramMap["usage"]
-
-    if usage:
-        params.usage()
-
+s = None
+for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
     try:
-        _, serverPort = re.split(":", server)
-        serverPort = int(serverPort)
-    except:
-        print("Can't parse server:port from '%s'" % server)
-        sys.exit(1)
+        print("creating sock: af=%d, type=%d, proto=%d\n" % (af, socktype, proto))
+        s = socket.socket(af, socktype, proto)
+    except socket.error as msg:
+        print(" error: %s\n" % msg)
+        s = None
+        continue
+    try:
+        print(" attempting to connect to %s\n" % repr(sa))
+        s.connect(sa)
+    except socket.error as msg:
+        print(" error: %s\n" % msg)
+        s.close()
+        s = None
+        continue
+    break
 
-    s = None
-    for res in socket.getaddrinfo(host, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
-        af, socktype, proto, canonname, sa = res
-        try:
-            print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
-            s = socket.socket(af, socktype, proto)
-        except socket.error as msg:
-            print(" error: %s" % msg)
-            s = None
-            continue
-        try:
-            print(" attempting to connect to %s" % repr(sa))
-            s.connect(sa)
-        except socket.error as msg:
-            print(" error: %s" % msg)
-            s.close()
-            s = None
-            continue
-        break
+if s is None:
+    print('could not open socket\n')
+    sys.exit(1)
 
-    if s is None:
-        print('could not open socket')
-        sys.exit(1)
-    
-    delay = float(paramMap['delay']) # delay before sending (default = 0s)
-    if delay != 0:
-        print(f"sleeping for {delay}s")
-        time.sleep(delay)
-        print("done sleeping")
-
-    return s
-
-
-def send_file(local_file, remote_file, host):
-    s = create_socket(host)
-    file_descriptor = os.open(local_file, os.O_RDONLY)
-    file_size = os.path.getsize(local_file)
-    chunk = os.read(file_descriptor, 1024)
-    s.send("{0}:s{1}".format(len(remote_file) + 1, remote_file).encode())
-    s.send("{0}:{1}".format(file_size, chunk.decode()).encode())
-    
-    while True:
-        chunk = os.read(file_descriptor, 1024)
-        if not chunk:
+framedSocket.sendMessage(s,sys.argv[0].encode())  # "Send" input
+framedSocket.sendMessage(s,serverFile.encode())   # file name to be saved in server
+response = framedSocket.receiveMessage(s)         # gets response from the server
+if(response == "OK"):
+    fd = os.open("./client/"+clientFile, os.O_RDONLY)
+    next = 0
+    limit = 0
+    buf = ""
+    message= ""
+    while 1:
+        buf = os.read(fd, 100).decode()
+        limit = len(buf)
+        if limit == 0:
             break
-        else:
-            s.send(chunk)
-    
-    s.close()
-
-
-def recieve_file(local_file, remote_file, host):
-    s = create_socket(host)
-    s.send("{0}:r{1}".format(len(remote_file) + 1, remote_file).encode())
-    data = s.recv(1024).decode()
-    delim = data.index(":")
-    msg_length = int(data[:delim])
-    data = data[delim + 1:]
-    file_descriptor = os.open(local_file, os.O_CREAT | os.O_WRONLY)
-    
-    full_msg = ""
-    while data:
-        full_msg += data
-        msg_length -= len(data)
-        data = s.recv(1024).decode()
-            
-        if msg_length == 0:
-            os.write(file_descriptor, full_msg.encode())
-            break
-    
-    s.close()
+        message += buf   # string buffer adds to real message to be sent
+    framedSocket.sendMessage(s, message.encode())   # message sent through a framed socket
+    result = framedSocket.receiveMessage(s)      # recieves a result of the transfer
+    print(result+"\n")                    # prints success result
+elif (response == "NO"): 
+    print("File name already taken on server file\n")
+else:
+    print("File currently being written into\n")
+s.close()
