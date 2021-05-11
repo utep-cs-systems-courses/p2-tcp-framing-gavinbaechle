8a7e08b4817555c9ec3dd0c2a97aa95d779b3a6c
gavinbaechle <77751057+gavinbaechle@users.noreply.github.com>
2021-05-10 19:08:17 -0600

    Update client.py

--- file_transfer/src/client.py
+++ file_transfer/src/client.py
@@ -10,7 +10,8 @@ progname = "fileClient"
 try:                            # tries to get the necessary parameters
     clientFile = sys.argv[1]        
     serverHost, serverPort = re.split(":", sys.argv[2])
-    serverPort = 50001
+    serverFile = sys.argv[3]
+  
 except:                         # if it fails, prints syntax.
     print("Bad param format: '%s'. Should be $ ./fileClient Send {clientFile} {host:serverFile}\n" % sys.argv)
     sys.exit(1)
